ee9ec2299e82f379176f65489eaaf14455e94807
iNoSec2 <piereaupjpeje@gmail.com>
2019-07-07 15:25:22 +0200

    refacto pour permettre de mettre le js a part dans un file et separation du php en plusieurs fichiers/founctions

--- index1.php
+++ index1.php
@@ -45,100 +45,12 @@
         </div>
     </div>
     <?php
-require_once("php/tweeter.php");
-tweet("$_POST[tweet]");
-
-$newnbtweet=file_get_contents("php/comptweet.txt");
-
-
-    if ($newnbtweet == 8){
-        require_once("php/reinit_files.php");
-        reinit(0,230);
-    }
-    else if ($newnbtweet == 7){
-        require_once("php/recup_tweet.php");
-        recupTweet("wakary11",5);
-
-        $vomiTXT = fopen('php/vomi.txt', 'r+'); /* ouvre le fichier comptuer.txt en mode r+ et range le dans la variable $compteur */
-
-        while (!feof($vomiTXT)) /* tant que le pointeur n'est pas a la fin du fichier () */
-        {
-            $vomi = fgets($vomiTXT); // lis les lignes du fichier()
-            require_once("php/tweeter.php");
-            tweet("$vomi");
-        }
-
-        ftruncate($vomiTXT,0); // efface le contenu du fichier apres avoir tweeter le vomi afin de repartir sur un fichier vide pour le prochain vomi
-        fclose($vomiTXT);
-
-        require_once("php/reinit_files.php");
-        reinit(0,230);
-    }
-    else{
-        file_put_contents("php/comptweet.txt",@file_get_contents("php/comptweet.txt")+1);//je put dans le fichier php/comptweet.txt le contenu de php/comptweet.txt (je lis le fichier avec file_get_contents) incementer de 1
-        $nbtweet = fopen('php/comptweet.txt', 'r+');
-        $newnbtweet= fgets($nbtweet); // lis les lignes du fichier()
-        fclose($nbtweet);
-
-        file_put_contents("php/newtaille.txt",@file_get_contents("php/newtaille.txt")+40);//pareil qu'avec comptweet.txt sauf que la je l'incremente de 40 a chaque fois
-        $newtailleLecture = fopen('php/newtaille.txt', 'r+');
-        $newtaille = fgets($newtailleLecture); // lis les lignes du fichier()
-        fclose($newtailleLecture);
-    }
+    require_once("php/all.php");
     ?>
-
     <script>
-        // !!!!!!!!!!! POUR RECUP MES VARIABLES PHP EN JS JE SUIS OBLIGÉ D'UTILISER ECHO ET JSON_ENCODE !!!!!!!
         var nbtweet = <?php echo json_encode($newnbtweet); ?>;// je recup ma variable php $newnbtweet et je le mets dans ma var JS nbtweet
-        var myImg = document.getElementById("tweetGo");
         var newtaille = <?php echo json_encode($newtaille); ?>; // pareil que nbtweet
-
-        myImg.style.width= newtaille + "px"; //j'applique ma variable newtaille au style en HTML a myImg
-        myImg.style.height= newtaille + "px" ;
-        if(nbtweet==8){
-            reinit();
-        }
-
-
-        function reinit(){
-            var currHeight = myImg.clientHeight;
-            var currWidth = myImg.clientWidth;
-            var debHeight = 270;
-            var debWidth = 270;
-            myImg.style.height= (debHeight) + "px"
-            myImg.style.width= (debWidth) + "px"
-            myImg.style.display="flex";
-            myImg.style.justifyContent="center";
-        }
-
-        var modal = document.getElementById("myModal");
-
-        // Get the button that opens the modal
-        var btn = document.getElementById("modalBtn");
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("modalclose")[0];
-
-
-
-        // bouton qui permet d'ouvrir le modal aprés n tweets
-        btn.onclick = function() {
-
-            if (nbtweet >= 5){
-                modal.style.display = "block";
-            }
-        }
-
-        // bouton qui permet de fermer le modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
     </script>
+    <script src="js/zoom_modal.js"></script>
 </body>
 </html>
